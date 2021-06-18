ebd570792f66d10d3b8a8956ed1f307c97bf1160
Dun Ma <madun1999@gmail.com>
2021-06-18 11:51:11 -0500

    naive version

--- main.py
+++ main.py
@@ -1,49 +1,107 @@
-def check(top_one: list[int], top_two: list[int], left_one: list[int], left_two: list[int], board: list[list[bool]]) -> bool:
-    return True # TODO: implement check
+from functools import cache
+from typing import Callable
 
 
-def solve(top_one: list[int], top_two: list[int], left_one: list[int], left_two: list[int],
-          board: list[list[bool]], x: int, y: int):
-    length = len(top_one) * 2 # total side length
-    if x >= length:
-        return check(top_one, top_two, left_one, left_two, board)
-    next_x = (x + 1) % length
-    next_y = y + (1 if x + 1 == length else 0)
-    board[x][y] = False
-    solve(top_one, top_two, left_one, left_two, board, next_x, next_y)
-    board[x][y] = True
-    solve(top_one, top_two, left_one, left_two, board, next_x, next_y)
+def one_func(x: bool) -> int:
+    return 1 if x else 0
 
 
+def two_func(x: bool) -> int:
+    return 2 if x else 0
 
-def inc(top_one: list[int], top_two: list[int], left_one: list[int], left_two: list[int]):
-    length = len(top_one)
-    minConstraint = 1
-    maxConstraint = 3*length
-    pass #TODO: implement inc
 
-def guess(length): # length is half the side length of all
-    top_one = [1] * length # non-increasing
-    top_two = [1] * length
-    left_one = [1] * length
-    left_two  = [1] * length
-    def skip() -> bool:
-        if sum(left_one) + sum(left_two) != sum(top_one) + sum(top_two):
-            return True
-        if sum(left_one) % 2 != sum(top_one) % 2:
-            return True
+@cache
+def getMatrix(length: int) -> list[list[Callable[[bool], int]]]:
+    return [[one_func if ((i < length) == (j < length)) else two_func for i in range(length * 2)] for j in
+            range(length * 2)]
+
+
+def check(top: list[int], left: list[int], board: list[list[bool]], length: int) -> bool:
+    board_funcs = getMatrix(length)
+    board_int = [[board_funcs[i][j](board[i][j]) for i in range(length * 2)] for j in range(length * 2)]
+    row_sum = [sum(row) for row in board_int]
+    if row_sum != left:
         return False
-    incorrect = 0
+    col_sum = [sum(row[i] for row in board_int) for i in range(length * 2)]
+    return col_sum == top
+
+
+def solve(top: list[int], left: list[int],
+          board: list[list[bool]], x: int, y: int, length: int) -> bool:
+    full_length = length * 2  # total side length
+    if y >= full_length:
+        return check(top, left, board, length)
+    next_x = (x + 1) % full_length
+    next_y = y + (1 if x + 1 == full_length else 0)
+    board[x][y] = False
+    if solve(top, left, board, next_x, next_y, length):
+        return True
+    board[x][y] = True
+    if solve(top, left, board, next_x, next_y, length):
+        return True
+    return False
+
+
+def skip(left: list[int], top: list[int], length: int) -> bool:
+    for i in range(length-1):  # non-increasing
+        if left[i] < left[i+1]:
+            return True
+        if left[i+length] < left[i+length+1]:
+            return True
+        if top[i] < top[i+1]:
+            return True
+        if top[i+length] < top[i+length+1]:
+            return True
+
+    if sum(left) != sum(top):  # sum constraint
+        return True
+    if (sum(left[:length]) % 2) != (sum(top[:length]) % 2):  # parity constraint
+        return True
+    return False
+
+
+def gen_constraints(length: int):
+    top = [1] * (length * 2)  # non-increasing
+    left = [1] * (length * 2)
+    length = len(top) // 2
+    min_constraint = 1
+    max_constraint = 3 * length
     while True:
-        if not skip():
-            board = [[False for _ in range(length * 2)] for _ in range(length * 2)]
-            solve(top_one, top_two, left_one, left_two, board, 0, 0)
-        if inc(top_one, top_two, left_two, left_two):
+        if not skip(left, top, length):
+            yield top, left
+        # gen next
+        carry = 1
+        for idx in range(length * 2):
+            if top[idx] + carry > max_constraint:
+                top[idx] = 1
+                continue
+            else:
+                top[idx] += 1
+                carry = 0
+                break
+        if carry == 1:
+            for idx in range(length * 2):
+                if left[idx] + carry > max_constraint:
+                    left[idx] = 1
+                    continue
+                else:
+                    left[idx] += 1
+                    carry = 0
+                    break
+        if carry == 1:
             break
-    print("Total incorrect: ", incorrect)
+
+
+def guess(length):  # length is half the side length of all
+    failed = 0
+    for left, top in gen_constraints(length):
+        board = [[False for _ in range(length * 2)] for _ in range(length * 2)]
+        if not solve(top, left, board, 0, 0, length):
+            print(top, left)
+            failed += 1
+    print(f"{failed=}")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     guess(2)
-
-
