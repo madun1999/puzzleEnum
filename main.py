c2d8211ab5a157d6e605ce4f460f1b5e39e39748
Dun Ma <madun1999@gmail.com>
2021-06-26 18:11:43 -0500

    speed up gen_constraints with partition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functools import cache
+from itertools import combinations
 from typing import Callable
 import cProfile
 
@@ -65,59 +66,92 @@ def solve(top: list[int], left: list[int],
 
 
 def skip(left: list[int], top: list[int], length: int) -> bool:
-    for i in range(length-1):  # skip if not non-increasing
-        if left[i] < left[i+1]:
-            return True
-        if left[i+length] < left[i+length+1]:
-            return True
-        if top[i] < top[i+1]:
-            return True
-        if top[i+length] < top[i+length+1]:
-            return True
+    # for i in range(length-1):  # skip if not non-increasing 
+    #     if left[i] > left[i+1]:
+    #         return True
+    #     if left[i+length] < left[i+length+1]:
+    #         return True
+    #     if top[i] < top[i+1]:
+    #         return True
+    #     if top[i+length] < top[i+length+1]:
+    #         return True
     if left == top: # skip if symmetric
         return True
-    if sum(left) != sum(top):  # skip if not sum constraint
-        return True
-    if (sum(left[:length]) % 2) != (sum(top[:length]) % 2):  # skip if not parity constraint
-        return True
+    # if sum(left) != sum(top):  # skip if not sum constraint
+    #     return True
+    # if (sum(left[:length]) % 2) != (sum(top[:length]) % 2):  # skip if not parity constraint
+    #     return True
     
     all_nums = list(set(left + top)) # skip if not almost regular
     if len(set(left + top)) > 2 or (all_nums[0] != all_nums[1] + 1 and all_nums[1] != all_nums[0] + 1):  
         return True
-
+               
     return False
+
+
+# Modified AccelAsc
+# Generating All Partitions: A Comaprison of Two Encodings
+# Secion 4.1.2
+# Kelleher, Jerome; O'Sullivan, Barry
+# prerequisite: length >= 2
+def partitions(sumi, length, arr, start):
+    for i in range(length): # clear array
+        arr[i+start] = 0
+    k = 1 # index
+    y = sumi - 1 # remaining sum
+    while k != 0:
+        k -= 1
+        x = arr[k + start] + 1 
+        while 2 * x <= y and k <= length - 3:
+            arr[k + start] = x
+            y -= x
+            k += 1
+        l = k + 1
+        while x <= y:
+            arr[k + start] = x
+            arr[l + start] = y
+            yield None
+            x += 1
+            y -= 1
+        y += x - 1
+        arr[k + start] = y + 1
+        arr[l + start] = 0
+        yield None
 
 
 def gen_constraints(length: int):
     min_constraint = 0
-    max_constraint = 3 * length
-    top = [min_constraint] * (length * 2)  # non-increasing
+    max_constraint = length * 3
+    top = [min_constraint] * (length * 2) 
     left = [min_constraint] * (length * 2)
-    length = len(top) // 2
+    min_sector_sum = min_constraint * length
+    max_sector_sum = max_constraint * length 
+    top_sectors = [0] * 2
+    left_sectors = [0] * 2
     while True:
-        if not skip(left, top, length):
-            yield top, left
-        # gen next
+        for left_sector_zero_sum in range(top_sectors[0] % 2, sum(top_sectors) + 1, 2):
+            left_sectors[0] = left_sector_zero_sum
+            left_sectors[1] = sum(top_sectors) - left_sector_zero_sum
+            for _ in partitions(top_sectors[0], length, top, 0):
+                for _ in partitions(top_sectors[1], length, top, length):
+                    for _ in partitions(left_sectors[0], length, left, 0):
+                        for _ in partitions(left_sectors[1], length, left, length):
+                            if not skip(left, top, length):   
+                                yield left, top
+        # gen next top_sectors
         carry = 1
-        for idx in range(length * 2):
-            if top[idx] + carry > max_constraint:
-                top[idx] = min_constraint
+        # add one to top
+        for idx in range(2):
+            if top_sectors[idx] + carry > max_sector_sum:
+                top_sectors[idx] = min_sector_sum
                 continue
             else:
-                top[idx] += 1
+                top_sectors[idx] += 1
                 carry = 0
                 break
         if carry == 1:
-            for idx in range(length * 2):
-                if left[idx] + carry > max_constraint:
-                    left[idx] = min_constraint
-                    continue
-                else:
-                    left[idx] += 1
-                    carry = 0
-                    break
-        if carry == 1:
             break
+
 
 
 def guess(length):  # length is half the side length of all
@@ -142,5 +176,7 @@ if __name__ == '__main__':
     # board = [[False for _ in range(length * 2)] for _ in range(length * 2)]
     # board = [[True, False, False, False], [False, True, False, False], [True, False, True, False], [False, False, False, True]]
     # print(solve([2,2,1,1],[1,1,2,2], board, 0, 0, length))
-
-    cProfile.run('guess(3)')
+    guess(3)
+    # cProfile.run('guess(3)')
+    # print(sorted([(1,2),(2,1)]))
+    
